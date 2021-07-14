6d20e78869dbb23a09f6e84b3b065ff2b8278ff0
reilly.wood <reilly.wood@icloud.com>
2021-07-13 21:50:13 -0700

    Add visualization of most frequent window messages

--- CsWin32Playground/Program.cs
+++ CsWin32Playground/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.Win32.SafeHandles;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.Graphics.Gdi;
@@ -16,6 +21,12 @@ namespace CsWin32Playground
     // https://www.gitmemory.com/issue/microsoft/CsWin32/244/822066737
     class Program
     {
+        private record WinMessage(uint msg, WPARAM wParam, LPARAM lParam);
+
+        static private List<WinMessage> messages = new();
+
+        static private Dictionary<uint, string> MessageNameLookup = WinMessageLookup.MessageNameDict();
+
         static unsafe void Main(string[] args)
         {
             // Microsoft.Windows.Sdk.PInvoke
@@ -68,42 +79,111 @@ namespace CsWin32Playground
             }
         }
 
+
         private static LRESULT WndProc(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam)
         {
             HDC hdc;
             PAINTSTRUCT ps;
             RECT rect;
 
+            messages.Add(new WinMessage(msg, wParam, lParam));
+
+            if (msg != Constants.WM_PAINT && messages.Count % 7 == 0)
+            {
+                RepaintWholeWindow(hwnd);
+            }
+
             switch (msg)
             {
-                case Constants.WM_CREATE:
-                    break;
-                case Constants.WM_SIZE:
-                    {
-                        int width = GetLowWord(lParam.Value);   // Get the low-order word.
-                        int height = GetHighWord(lParam.Value); // Get the high-order word.
+                //case Constants.WM_SIZE:
+                //    {
+                //        int width = GetLowWord(lParam.Value);   // Get the low-order word.
+                //        int height = GetHighWord(lParam.Value); // Get the high-order word.
 
-                        // Respond to the message:
-                        OnSize(hwnd, wParam, width, height);
-                    }
-                    break;
+                //        return (LRESULT) 0;
+                //    }
                 case Constants.WM_MOUSEMOVE:
-                    Console.WriteLine(nameof(Constants.WM_MOUSEMOVE));
-                    break;
+
+                    return (LRESULT) 0;
                 case Constants.WM_PAINT:
-                    hdc = PInvoke.BeginPaint(hwnd, out ps);
-                    PInvoke.GetClientRect(hwnd, out rect);
-
-                    var hdcHandle = new HdcSafeHandle(hdc);
-
-                    PInvoke.DrawText(hdcHandle, "Hello World", -1, ref rect, DRAW_TEXT_FORMAT.DT_SINGLELINE |  DRAW_TEXT_FORMAT.DT_CENTER | DRAW_TEXT_FORMAT.DT_VCENTER);
-                    PInvoke.EndPaint(hwnd, ps);
-                    break;
+                    Paint(hwnd, out hdc, out ps, out rect);
+                    return (LRESULT) 0;
                 default:
                     break;
             }
 
+            //RepaintWholeWindow(hwnd);
+
             return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
+        }
+
+        private static string FormatMessagesExpensive(IEnumerable<WinMessage> messages)
+        {
+            StringBuilder ret = new();
+            ret.AppendLine("Most Popular Window Messages:");
+            ret.AppendLine();
+
+            var grouped = messages.GroupBy((m) => m.msg)
+                .Select(g => new { MsgName = GetPossibleMethodNames(g.Key), MsgCode = g.Key, Count = g.Count() })
+                .OrderByDescending(m => m.Count)
+                .Take(40);
+
+            foreach (var item in grouped)
+            {
+
+                string cnt = $"{item.Count}x".PadRight(10, ' ');
+
+                ret.Append(cnt);
+                ret.Append(cnt.Length);
+                ret.AppendLine($"{item.MsgName} (0x{item.MsgCode})");
+            }
+
+            return ret.ToString();
+
+            static string GetPossibleMethodNames(uint msgCode)
+            {
+                return MessageNameLookup.ContainsKey(msgCode) ? MessageNameLookup[msgCode] : $"Unknown (0x{msgCode:x})";
+            }
+        }
+
+        private static void RepaintWholeWindow(HWND hwnd)
+        {
+            RECT wholeWindow;
+            PInvoke.GetClientRect(hwnd, out wholeWindow);
+
+            // erasing the background is slow and can cause flicker. only do it infrequently
+            bool eraseBackground = messages.Count % 2 == 0;
+            PInvoke.InvalidateRect(hwnd, wholeWindow, true);
+            PInvoke.UpdateWindow(hwnd);
+        }
+
+        private static void Paint(HWND hwnd, out HDC hdc, out PAINTSTRUCT ps, out RECT rect)
+        {
+            hdc = PInvoke.BeginPaint(hwnd, out ps);
+            PInvoke.GetClientRect(hwnd, out rect);
+
+            var hdcHandle = new HdcSafeHandle(hdc);
+
+            string displayText = $"Message count: {messages.Count}" + Environment.NewLine + Environment.NewLine;
+            
+            displayText += FormatMessagesExpensive(messages);
+
+            RECT offsetRect = rect;
+            int offsetPx = 20;
+            offsetRect.left += offsetPx;
+            offsetRect.top += offsetPx;
+            offsetRect.right -= offsetPx;
+            offsetRect.bottom -= offsetPx;
+
+            HGDIOBJ hFont, hOldFont;
+
+            hFont =  PInvoke.GetStockObject(GET_STOCK_OBJECT_FLAGS.ANSI_FIXED_FONT);
+
+            hOldFont = PInvoke.SelectObject(hdc, hFont);
+            PInvoke.DrawText(hdcHandle, displayText, -1, ref offsetRect, DRAW_TEXT_FORMAT.DT_LEFT | DRAW_TEXT_FORMAT.DT_VCENTER);
+            PInvoke.SelectObject(hdc, hOldFont);
+
+            PInvoke.EndPaint(hwnd, ps);
         }
 
         private static void OnSize(HWND hwnd, WPARAM wParam, int width, int height)
@@ -115,17 +195,13 @@ namespace CsWin32Playground
         {
             uint xy = (uint)value;
             int x = unchecked((short)xy);
-            int y = unchecked((short)(xy >> 16));
-
             return x;
         }
 
         private static int GetHighWord(nint value)
         {
             uint xy = (uint)value;
-            int x = unchecked((short)xy);
             int y = unchecked((short)(xy >> 16));
-
             return y;
         }
 
