a9b33b3df05b5989a90fad5990f2c93bc411fbbe
Reilly Wood <reilly.wood@icloud.com>
2021-10-29 22:08:17 -0700

    Add HotReloadManager

---
+++ HotReloadManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection.Metadata;
+using MinimalWebView;
+using System.Diagnostics;
+
+[assembly: MetadataUpdateHandler(typeof(HotReloadManager))]
+
+namespace MinimalWebView
+{
+    internal static class HotReloadManager
+    {
+        public static void ClearCache(Type[]? types)
+        {
+            Debug.WriteLine("ClearCache");
+        }
+
+        public static void UpdateApplication(Type[]? types)
+        {
+            Debug.WriteLine("UpdateApplication");
+        }
+    }
+}
